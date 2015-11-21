4f531d3a9458db31d04aff3a18895660a61e4fe6
timcmiller <timcmiller@live.com>
2015-11-21 13:43:34 -0800

    started added auth

---
+++ tim_miller/lib/basic_http.js
@@ -0,0 +1,17 @@
+var basicHttp = module.exports = exports = function(req, res, next) {
+  try{
+    var authString = req.header.authorization;
+    var basicString = authString.split(' ')[1];
+    basicString = basicString || new Buffer(':').toString('base64');
+    var basicBuffer = new Buffer(basicString, 'base64');
+    var authArray = basicBuffer.toString().split(':');
+    req.auth = {
+      username: authArray[0],
+      password: authArray[1]
+    };
+    next();
+  } catch(e) {
+    console.log(e);
+    return res.status(401).json({msg: 'What chu talking bout Willis'});
+  }
+};
