0c38b36041e5d583f521d259380e1ccb0753cd6f
timcmiller <timcmiller@live.com>
2015-12-04 18:46:54 -0800

    added some services and stuff

--- tim_miller/server.js
+++ tim_miller/server.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var express = require('express');
-var fs = require('fs');
 var officerRouter = require(__dirname + '/routes/officerroutes.js');
 var felonRouter = require(__dirname + '/routes/felonroutes.js');
 var bustedRouter = require(__dirname + '/routes/bustedroutes.js');
@@ -11,26 +10,12 @@ var app = express();
 process.env.APP_SECRET = process.env.APP_SECRET || '123451234512345';
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/officer_dev');
 
-app.use('/api', officerRouter, felonRouter, bustedRouter, chuckRouter, authRouter);
+app.use('/api', officerRouter);
 app.use('/api', felonRouter);
 app.use('/api', bustedRouter);
 app.use('/api', chuckRouter);
 app.use('/api', authRouter);
 app.use(express.static(__dirname + '/build'));
-
-
-// app.get('/:filename', function(req, res, next) {
-//   fs.stat(__dirname + '/build/' + req.params.filename, function(err, stats) {
-//     if (err) {console.log(err);
-//       return next();
-//     }
-
-//     if (!stats.isFile()) return next();
-
-//     var file = fs.createReadStream(__dirname + '/build/' + req.params.filename);
-//     file.pipe(res);
-//   });
-// });
 
 app.use(function(req, res) {
   res.status(404).send('could not find file');
