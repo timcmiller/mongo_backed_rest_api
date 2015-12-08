ac54a7dcb5cc858828cb5128db19d109e989b428
timcmiller <timcmiller@live.com>
2015-12-07 20:54:16 -0800

    added some sassyness

--- tim_miller/gulpfile.js
+++ tim_miller/gulpfile.js
@@ -3,10 +3,10 @@ var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var webpack = require('webpack-stream');
 var minifyCss = require('gulp-minify-css');
-var concatCss = require('gulp-concat-css');
 var gulpWatch = require('gulp-watch');
-
-gulp.task('default', ['jshint', 'test']);
+var sass = require('gulp-sass');
+var concatCSS = require('gulp-concat-css');
+var maps = require('gulp-sourcemaps');
 
 gulp.task('jshint', function(){
   gulp.src(['gulpfile.js', 'lib/*.js', 'test/*.js', 'server.js', 'models/*.js', 'routes/*.js'])
@@ -24,21 +24,26 @@ gulp.task('static:dev', function() {
   .pipe(gulp.dest('build/'));
 });
 
-gulp.task('css:dev', function() {
-  return gulp.src([
-    'app/css/base.css',
-    'app/css/skeleton.css',
-    'app/css/state.css',
-    'app/css/layout.css',
-    'app/css/module.css',
-    ])
-    .pipe(concatCss('styles.min.css'))
+gulp.task('sass:dev', function() {
+    gulp.src('./app/sass/application.scss')
+    .pipe(maps.init())
+    .pipe(sass().on('error', sass.logError))
     .pipe(minifyCss())
+    .pipe(maps.write('./'))
     .pipe(gulp.dest('build/'));
 });
 
+gulp.task('css:dev', function() {
+  return gulp.src([
+    'app/css/base.css',
+    'app/css/skeleton.css'])
+  .pipe(concatCSS('styles.min.css'))
+  .pipe(minifyCss())
+  .pipe(gulp.dest('build/'));
+});
+
 gulp.task('watch', function() {
-  gulp.watch('./app/**', ['default']);
+  gulp.watch('./app/**/*', ['styles']);
 });
 
 gulp.task('webpack:dev', function() {
@@ -61,6 +66,6 @@ gulp.task('webpack:test', function() {
   .pipe(gulp.dest('test/client/'));
 });
 
-
+gulp.task('styles', ['sass:dev', 'css:dev']);
 gulp.task('build:dev', ['webpack:dev', 'static:dev', 'css:dev']);
-gulp.task('default', ['build:dev']);
+gulp.task('default', ['build:dev', 'jshint', 'test']);
